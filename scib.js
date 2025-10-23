061584e26a442f6a20ffaf85ea2231be43f2e861
baim18092008-hub <baim18092008@gmail.com>
2025-10-23 10:11:42 +0700

    Create scib.js

---
+++ scib.js
@@ -0,0 +1,7 @@
+//1. pilih tombol berdasarkan ID-nya
+const tombolsapa = document.getELemenByid['sapaButton'];
+//2. tambahkan 'event listener' untuk aksi 'click'
+tombolsapa.addEventlistener['click',function[] {
+//3.jalankan kode ini ketika tombol di-klik
+alert['Halo!Trima kasi sudah berkunjung!];
+  }];
